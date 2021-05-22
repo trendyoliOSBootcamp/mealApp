1ffd08e6352e303ecec383ecddc7617b97f3aaa7
Sule Turp <sule.turp@trendyol.com>
2021-05-22 13:34:02 +0300

    add restaurant collection view cell

---
+++ MealApp/MealApp/RestaurantCollectionViewCell/RestaurantCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantCollectionViewCell.swift
+//  MealApp
+//
+//  Created by Şule Turp on 22.05.2021.
+//
+
+import UIKit
+
+class RestaurantCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- MealApp/MealApp/ViewController.swift
+++ MealApp/MealApp/ViewController.swift
@@ -10,9 +10,14 @@ import CoreApi
 
 class ViewController: UIViewController {
     let networkManager: NetworkManager<HomeEndpointItem> = NetworkManager()
+    @IBOutlet private weak var widgetsCollectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchWidgets()
+    }
+
+    private func fetchWidgets() {
         networkManager.request(endpoint: .homepage(query: "page=1"), type: HomeResponse.self) { result in
             switch result {
             case .success(let response):
