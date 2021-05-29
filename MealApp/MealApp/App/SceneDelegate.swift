5b6cbf70914333aa5971eecf34914eb76422d8fe
Emre <emre.ergun@trendyol.com>
2021-05-29 09:53:04 +0300

    Change initialization at the scene delegate

--- MealApp/MealApp/App/SceneDelegate.swift
+++ MealApp/MealApp/App/SceneDelegate.swift
@@ -17,12 +17,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        let windowScene = UIWindowScene(session: session, connectionOptions: connectionOptions)
-        self.window = UIWindow(windowScene: windowScene)
-        let viewController = HomeRouter.createModule()
-        self.window?.rootViewController = viewController
-        self.window?.makeKeyAndVisible()
-        guard let _ = (scene as? UIWindowScene) else { return }
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = HomeRouter.createModule()
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
